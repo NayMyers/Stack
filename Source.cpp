9d535aaaab841bc96b82ac5e0e398f7d522de808
Ryan Syme <33377621+NayMyers@users.noreply.github.com>
2018-02-06 12:26:08 +0000

    Add files via upload
    Added more comments, tidied formatting a little

--- Source.cpp
+++ Source.cpp
@@ -3,47 +3,51 @@
 
 using namespace std;
 
-template<typename T>
+
 class Node
 {
 public:
-	T value;
+	int value;
 	Node* pNext = nullptr;
 };
 
-template<typename T>
+
 class Stack
 {
 private:
 	int count = 0;
-	Node<T>* pTop = nullptr;
-	Node<T>* nodePop(void);
+	Node* pTop = nullptr;
+	Node* nodePop(void);
 public:
 	~Stack(void);
-	void push(T value);
-	T pop(void);
+	void push(int value);
+	int pop(void);
 	int size(void);
-	
+
 };
-template<typename T>
-Node<T>* Stack<T>::nodePop(void) //Switches Ptop to the next value in the chain and makes pTmp equal to the top of the chain.
+int Stack::size()
 {
-	Node<T>* pTmp = pTop;
-	if (pTop != nullptr) pTop = pTop->pNext;
-	return pTmp;
+	return count;
 }
-template<typename T>
-T Stack<T>::pop(void)
+Node* Stack::nodePop(void) //Switches Ptop to the next value in the chain and makes pTmp equal to the top of the chain.
 {
-	if (count == 0) 
-	{ 
-		throw "Stack Empty!"; 
+	Node* pTmp = pTop; //creates a new pointer of type Node and makes it point to the same address as Ptop. This means pTmp is now 
+	//pointing to the top of the stack. This means when we move pTop to the next Node along we don't lose the top node causing a 
+	//memory leak.
+	if (pTop != nullptr) pTop = pTop->pNext;
+	return pTmp; //essentially return the address that pTop was pointing to
+}
+int Stack::pop(void)
+{
+	if (count == 0) // this means there are no more nodes on the chain and the program needs to exit to the catch blocks 
+	{
+		throw "Stack Empty!";
 	}
 
-	Node<T> *pTmp = nodePop(); // makes pTmp equal to the top of the chain and moves pTop to the next node along. 
-	T returnVal; // makes value to store value of node in
+	Node* pTmp = nodePop(); // makes pTmp equal to the top of the chain and makes pTop point to the next node along. 
+	int returnVal; // makes value to store value of node in
 
-	if (pTmp != nullptr)
+	if (pTmp != nullptr) // if there are still nodes left, return the value.
 	{
 		returnVal = pTmp->value;
 	}
@@ -52,45 +56,48 @@ T Stack<T>::pop(void)
 		throw "Error!";
 	}
 
-	delete pTmp;
-	count--;
+	delete pTmp; //deletes the temporary node so there is no memory leak
+	count--; // decrements the count because a value has been removed from the stack. 
 	return returnVal;
 }
-template<typename T>
-void Stack<T>::push(T value) // creates a new node makes it value equal to the one passed to the method. Makes it's pointer point to the current top of the chain then makes pTop point to the new top.
+void Stack::push(int value) // creates a new node makes it value equal to the one passed to the method. Makes it's pointer point to the current top of the chain then makes pTop point to the new top.
 {
-	Node<T>* pTmp = new Node<T>; //Creating new node
-	pTmp->value = value; //filling the node with the value 
-	pTmp->pNext = pTop; // makes the node point to the current top of the chain 
+	Node* pTmp = new Node; //Creating new pointer of type node called pTmp which points to a node object 
+	//This temporary node is used to take the new value point to the top of the stack and become part of the stack.
+	pTmp->value = value; //filling the node with the value (The value in the node object pointed to by the pTmp pointer will become the 
+	//value passed into the push method)
+	pTmp->pNext = pTop; // makes the new temporary node point to the current top of the chain 
 	pTop = pTmp; // makes pTop now point at the newly added node 
-	count++;
+	count++; //incriments the count so the program knows how many items are on the stack so it can throw an error if too many are popped.
 }
-template<typename T>
-Stack<T>::~Stack(void)
+
+Stack::~Stack(void) //deconstructor that runs when the stack object is deleted
 {
-	while (pTop != nullptr) { delete nodePop(); } //deletes all elements in the stack
+	while (pTop != nullptr) { delete nodePop(); } //deletes all elements in the stack 
 }
-
-
 
 int main()
 {
+	Stack* Stack1 = new Stack;
 	try
 	{
-		Stack<int> Stack1;
-		Stack1.push(5);
-		Stack1.push(9);
-		cout << "Value 1: " << Stack1.pop() << endl;
-		cout << "Value 2: " << Stack1.pop() << endl;
+		for (int x = 0; x < 10; x++)
+		{
+			Stack1->push(x);
+		}
+		while (true) // This will exit when the throw is reached inside the method pop and fall to the next availible relevant catch 
+		{             // block
+			cout << Stack1->pop() << " ";
+		}
 	}
 	catch (char* errorMessage)
 	{
 		cout << errorMessage << endl;
 	}
-	catch (...)
+	catch (...) //Will be the default for any data type if it hasn't allready been caught by a previous catch block. 
 	{
 		cout << "There has been an unexpected error" << endl;
 	}
-	
+	delete Stack1;
 	return 0;
 }
