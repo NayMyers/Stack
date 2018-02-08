54f35ceedc1daede420be9243d51ea56b3b19b42
Ryan Syme <33377621+NayMyers@users.noreply.github.com>
2018-02-08 16:44:47 +0000

    Add files via upload
    Working Template stack with main function need to add template relevant comments

--- Source.cpp
+++ Source.cpp
@@ -4,45 +4,45 @@
 using namespace std;
 
 template<typename T>
-class Node
+class TemplateNode
 {
 public:
 	T value;
-	Node* pNext = nullptr;
+	TemplateNode* pNext = nullptr;
 };
 
 template<typename T>
-class Stack
+class TemplateStack
 {
 private:
 	int count = 0;
-	Node<T>* pTop = nullptr;
-	Node<T>* nodePop(void);
+	TemplateNode<T>* pTop = nullptr;
+	TemplateNode<T>* nodePop(void);
 public:
-	~Stack(void);
-	void push(T value);
-	T pop(void);
-	int size(void);
-	
+	~TemplateStack(void);
+	void Templatepush(T value);
+	T Templatepop(void);
+	int Templatesize(void);
+
 };
 template<typename T>
-Node<T>* Stack<T>::nodePop(void) //Switches Ptop to the next value in the chain and makes pTmp equal to the top of the chain.
+TemplateNode<T>* TemplateStack<T>::nodePop(void) //Switches Ptop to the next value in the chain and makes pTmp equal to the top of the chain.
 {
-	Node<T>* pTmp = pTop; //creates a new pointer of type Node and makes it point to the same address as Ptop. This means pTmp is now 
+	TemplateNode<T>* pTmp = pTop; //creates a new pointer of type Node and makes it point to the same address as Ptop. This means pTmp is now 
 	//pointing to the top of the stack. This means when we move pTop to the next Node along we don't lose the top node causing a 
 	//memory leak.
 	if (pTop != nullptr) pTop = pTop->pNext;
 	return pTmp; //essentially return the address that pTop was pointing to
 }
 template<typename T>
-T Stack<T>::pop(void)
+T TemplateStack<T>::Templatepop(void)
 {
 	if (count == 0) // this means there are no more nodes on the chain and the program needs to exit to the catch blocks 
-	{ 
-		throw "Stack Empty!"; 
+	{
+		throw "Stack Empty!";
 	}
 
-	Node<T>* pTmp = nodePop(); // makes pTmp equal to the top of the chain and moves pTop to the next node along. 
+	TemplateNode<T>* pTmp = nodePop(); // makes pTmp equal to the top of the chain and moves pTop to the next node along. 
 	T returnVal; // makes value to store value of node in
 
 	if (pTmp != nullptr)
@@ -59,16 +59,16 @@ T Stack<T>::pop(void)
 	return returnVal;
 }
 template<typename T>
-void Stack<T>::push(T value) // creates a new node makes it value equal to the one passed to the method. Makes it's pointer point to the current top of the chain then makes pTop point to the new top.
+void TemplateStack<T>::Templatepush(T value) // creates a new node makes it value equal to the one passed to the method. Makes it's pointer point to the current top of the chain then makes pTop point to the new top.
 {
-	Node<T>* pTmp = new Node<T>; //Creating new node
+	TemplateNode<T>* pTmp = new TemplateNode<T>; //Creating new node
 	pTmp->value = value; //filling the node with the value 
 	pTmp->pNext = pTop; // makes the node point to the current top of the chain 
 	pTop = pTmp; // makes pTop now point at the newly added node 
 	count++; //incriments the count so the program knows how many items are on the stack so it can throw an error if too many are popped.
 }
 template<typename T>
-Stack<T>::~Stack(void) //deconstructor that runs when the stack object is deleted
+TemplateStack<T>::~TemplateStack(void) //deconstructor that runs when the stack object is deleted
 {
 	while (pTop != nullptr) { delete nodePop(); } //deletes all elements in the stack 
 }
@@ -77,16 +77,16 @@ Stack<T>::~Stack(void) //deconstructor that runs when the stack object is delete
 
 int main()
 {
-	Stack<int>* Stack1 = new Stack<int>;
+	TemplateStack<int>* Stack1 = new TemplateStack<int>;
 	try
 	{
 		for (int x = 0; x < 10; x++)
 		{
-			Stack1->push(x);
+			Stack1->Templatepush(x);
 		}
 		while (true) // This will exit when the throw is reached inside the method pop and fall to the next availible relevant catch 
 		{             // block
-			cout << Stack1->pop() << " ";
+			cout << Stack1->Templatepop() << " ";
 		}
 	}
 	catch (char* errorMessage)
